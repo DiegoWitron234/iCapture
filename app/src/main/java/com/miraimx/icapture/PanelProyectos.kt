a78d3b86ea6ed9262335becf64f1bbcf11ff05d6
diego <maximusdiegoalonsodbz@gmail.com>
2023-11-22 14:59:46 -0600

    Versión retocada para mejorar la interfaz

--- app/src/main/java/com/miraimx/icapture/PanelProyectos.kt
+++ app/src/main/java/com/miraimx/icapture/PanelProyectos.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,10 +14,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -32,6 +38,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -60,14 +67,13 @@ class PanelProyectos : ComponentActivity() {
     }
 }
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PanelProyectosScreen() {
-    val context = LocalContext.current
     var showDialog by remember { mutableStateOf(false) }
     var projectName by remember { mutableStateOf(TextFieldValue("")) }
-    //var selectedProject by remember { mutableStateOf<String?>(null) }
+    var selectedProject by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -75,127 +81,79 @@ fun PanelProyectosScreen() {
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { showDialog = true }) {
-            Text("Crear Proyecto")
+        // Botón para crear un nuevo proyecto
+        IconButton(onClick = { showDialog = true }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Crear Proyecto")
         }
-        Spacer(modifier = Modifier.height(8.dp))
 
+        Spacer(modifier = Modifier.height(16.dp))
 
-        ProjectListScreen()
+        // Lista de proyectos
+        ProjectListScreen(onItemClick = { selectedProject = it })
 
-
+        // Diálogo para crear un nuevo proyecto
         if (showDialog) {
-            AlertDialog(
-                onDismissRequest = { showDialog = false },
-                title = { Text("Nuevo Proyecto") },
-                text = {
-                    OutlinedTextField(
-                        value = projectName,
-                        onValueChange = { projectName = it },
-                        label = { Text("Nombre del Proyecto") }
-                    )
-                },
-                confirmButton = {
-                    TextButton(onClick = {
-                        if (projectName.text.isNotBlank()) {
-                            // Agregar el código para crear un nuevo proyecto
-                            createNewProject(projectName.text)
-                            showDialog = false
-                        } else {
-                            // Mostrar un mensaje al usuario indicando que el nombre del proyecto no puede estar en blanco
-                            Toast.makeText(context, "El nombre del proyecto no puede estar en blanco", Toast.LENGTH_SHORT).show()
-                        }
-                    }) {
-                        Text("Aceptar")
+            CreateProjectDialog(
+                projectName = projectName,
+                onValueChange = { projectName = it },
+                onConfirmClick = {
+                    if (projectName.text.isNotBlank()) {
+                        // Agregar el código para crear un nuevo proyecto
+                        createNewProject(projectName.text)
+                        showDialog = false
+                    } else {
+                        // Mostrar un mensaje al usuario indicando que el nombre del proyecto no puede estar en blanco
+                        Toast.makeText(context, "El nombre del proyecto no puede estar en blanco", Toast.LENGTH_SHORT).show()
                     }
                 },
-                dismissButton = {
-                    TextButton(onClick = { showDialog = false }) {
-                        Text("Cancelar")
-                    }
-                }
+                onDismissClick = { showDialog = false }
             )
         }
     }
 }
 
-/*
-@Composable
-fun ProjectList(onItemClick: (String) -> Unit) {
-    val currentUserUid = FirebaseAuth.getInstance().currentUser?.uid
-    val projectList: MutableList<String> = mutableListOf()
-
-    if (currentUserUid != null) {
-        val databaseReference: DatabaseReference =
-            FirebaseDatabase.getInstance().getReference("$currentUserUid")
-
-        databaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                for (projectSnapshot in snapshot.children) {
-                    // Agregar el nombre del proyecto a la lista
-                    projectList.add(projectSnapshot.key!!)
-                    Log.d("Mensaje", "Proyecto encontrado: ${projectSnapshot.key}")
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // Manejar errores, si es necesario
-                println("Error al obtener la lista de proyectos: ${error.message}")
-            }
-        })
-    }
-
-    // Mostrar la lista de proyectos
-    // onItemClick se llama cuando se hace clic en un elemento
-    LazyColumn(
-        modifier = Modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(8.dp)
-    ) {
-        items(projectList) { project ->
-            TextButton(
-                onClick = { onItemClick(project) },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(project)
-            }
-            Divider()
-        }
-    }
-}
- */
-
 @Composable
 fun ProjectList(projects: List<String>, onItemClick: (String) -> Unit) {
     // Mostrar la lista de proyectos
-    // onItemClick se llama cuando se hace clic en un elemento
     LazyColumn(
         modifier = Modifier.fillMaxWidth(),
         contentPadding = PaddingValues(8.dp)
     ) {
         items(projects) { project ->
-            TextButton(
-                onClick = { onItemClick(project) },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(project)
-            }
-            Divider()
+            ProjectListItem(project = project, onItemClick = onItemClick)
         }
     }
 }
 
 @Composable
-fun ProjectListScreen() {
-    var selectedProject by remember { mutableStateOf("") }
-    val projectList by getProjectListForUser().observeAsState(initial = emptyList())
-
-    if (projectList.isEmpty()) {
-        Text("Aún no se han creado proyectos")
-    } else {
-        ProjectList(projectList) { selectedProject = it }
+fun ProjectListItem(project: String, onItemClick: (String) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = project,
+            modifier = Modifier.weight(1f),
+            style = MaterialTheme.typography.bodyMedium
+        )
+        IconButton(onClick = { onItemClick(project) }) {
+            Icon(imageVector = Icons.Default.Edit, contentDescription = "Editar")
+        }
     }
 }
 
+@Composable
+fun ProjectListScreen(onItemClick: (String) -> Unit) {
+    val projectList by getProjectListForUser().observeAsState(initial = emptyList())
+
+    if (projectList.isEmpty()) {
+        Text("Aún no se han creado proyectos", style = MaterialTheme.typography.bodyMedium)
+    } else {
+        ProjectList(projects = projectList, onItemClick = onItemClick)
+    }
+}
 
 fun getProjectListForUser(): LiveData<List<String>> {
     val currentUserUid = FirebaseAuth.getInstance().currentUser?.uid
@@ -225,6 +183,36 @@ fun getProjectListForUser(): LiveData<List<String>> {
     return projectList
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateProjectDialog(
+    projectName: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    onConfirmClick: () -> Unit,
+    onDismissClick: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismissClick,
+        title = { Text("Nuevo Proyecto") },
+        text = {
+            OutlinedTextField(
+                value = projectName,
+                onValueChange = onValueChange,
+                label = { Text("Nombre del Proyecto") }
+            )
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirmClick) {
+                Text("Aceptar")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissClick) {
+                Text("Cancelar")
+            }
+        }
+    )
+}
 
 // Agregar el nuevo proyecto a la base de datos
 fun createNewProject(projectName: String) {
