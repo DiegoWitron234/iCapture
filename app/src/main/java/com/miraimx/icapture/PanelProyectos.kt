bdac433f4fcf7d1411d766f99541fdee5b734366
diego <maximusdiegoalonsodbz@gmail.com>
2023-11-21 22:41:44 -0600

    Se agregó la segunda Activity a medias, todavía faltan cosas

--- app/src/main/java/com/miraimx/icapture/PanelProyectos.kt
+++ app/src/main/java/com/miraimx/icapture/PanelProyectos.kt
@@ -3,13 +3,30 @@ package com.miraimx.icapture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.miraimx.icapture.ui.theme.ICaptureTheme
 
 class PanelProyectos : ComponentActivity() {
@@ -22,25 +39,66 @@ class PanelProyectos : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PanelProyectosScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PanelProyectosScreen() {
+    val context = LocalContext.current
+    var showDialog by remember { mutableStateOf(false) }
+    var projectName by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { showDialog = true }) {
+            Text("Crear")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        // Agregar el código para mostrar la lista de proyectos
+        // Diego no ha programado esto porque se enfermó
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                title = { Text("Nuevo proyecto") },
+                text = {
+                    OutlinedTextField(
+                        value = projectName,
+                        onValueChange = { projectName = it },
+                        label = { Text("Nombre del proyecto") }
+                    )
+                },
+                confirmButton = {
+                    TextButton(onClick = {
+                        // Agregar el código para crear un nuevo proyecto
+                        // Diego no ha programado esto porque se enfermó
+                        showDialog = false
+                    }) {
+                        Text("Aceptar")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = { showDialog = false }) {
+                        Text("Cancelar")
+                    }
+                }
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PanelProyectosScreenPreview() {
     ICaptureTheme {
-        Greeting("Android")
+        PanelProyectosScreen()
     }
 }
